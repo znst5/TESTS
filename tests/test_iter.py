e9f77e7b98f8a01e426a6515d10d977d491bb6d0
znst5 <161199179+znst5@users.noreply.github.com>
2025-11-08 08:04:00 +0300

    q

---
+++ tests/test_iter.py
@@ -0,0 +1,34 @@
+import pytest
+
+from iter import FlatIterator
+
+
+
+
+def test_iter():
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+
+def test_iter1():
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+@pytest.mark.xfail
+def test_iter2():
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, 0]
+
+
+
+list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+
+
+
